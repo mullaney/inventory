fbe9f3a88b55783c3767eaf57ed476fbb705852b
Kevin Kelly <ircmullaney@gmail.com>
2014-10-30 19:21:17 -0500

    order current

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,7 +1,7 @@
     class ReportsController < ApplicationController
 
       def current
-        @items = Item.all.joins(:transactions).select("items.*, sum(transactions.amount) as transactions_sum").group("items.id").sort('name ASC')
+        @items = Item.all.joins(:transactions).select("items.*, sum(transactions.amount) as transactions_sum").group("items.id").order('items.name ASC')
       end
 
     end
