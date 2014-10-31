71b7f8fb82e16444214a4503e549dffd33d16a0c
Kevin Kelly <ircmullaney@gmail.com>
2014-10-30 19:15:19 -0500

    sort report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,8 +1,7 @@
     class ReportsController < ApplicationController
 
       def current
-        # @item_list = Transaction.uniq.pluck(:item_id)
-        @items = Item.all.joins(:transactions).select("items.*, sum(transactions.amount) as transactions_sum").group("items.id")
+        @items = Item.all.joins(:transactions).select("items.*, sum(transactions.amount) as transactions_sum").group("items.id").sort('name ASC')
       end
 
     end
