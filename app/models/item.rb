91f313bba61e186716f457cc1d306b120b785b4a
Kevin Kelly <ircmullaney@gmail.com>
2014-12-11 16:42:44 -0600

    wks left

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,21 @@ class Item < ActiveRecord::Base
   validates :name, presence: true
 
   scope :with_balance, -> { joins(:transactions).select("items.*, sum(transactions.amount) as transactions_sum").group("items.id").order('items.name ASC') }
+
+  def first_delivery_date
+    deliveries = self.transactions.where(code: 'delivery').order('date ASC').limit(1)
+    if !deliveries.empty?
+      deliveries.first.date
+    else
+      nil
+    end
+  end
+
+  def total_sales
+    sales = -self.transactions.where(code: 'sale').sum(:amount)
+  end
+
+  def recent_sales
+    sales = -self.transactions.where("code = ? AND date > ?", "sale", Date.today - 4.weeks).sum(:amount)
+  end
 end
